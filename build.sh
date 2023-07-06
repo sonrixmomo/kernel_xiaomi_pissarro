461851aea24d256da9bfdb558d8e9e3f90c00b84
Wahid Khan <wahidzk0091@gmail.com>
2023-07-06 08:31:16 +0000

    Build.sh: Update for pissarro
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-function compile() 
+function compile()
 {
 
 source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
 ccache -M 100G
 export ARCH=arm64
-export KBUILD_BUILD_HOST=MARKxDEVS
-export KBUILD_BUILD_USER="AbzRaider"
+export KBUILD_BUILD_HOST=NoVA
+export KBUILD_BUILD_USER="Abdul7852"
 git clone --depth=1  https://gitlab.com/LeCmnGend/proton-clang.git -b clang-13  clang
 
 
@@ -18,7 +18,7 @@ echo "Kernel OUT Directory Not Found . Making Again"
 mkdir out
 fi
 
-make O=out ARCH=arm64 RM6785_defconfig
+make O=out ARCH=arm64 pissarro_user_defconfig
 
 PATH="${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}" \
 make -j$(nproc --all) O=out \
@@ -34,17 +34,17 @@ make -j$(nproc --all) O=out \
 		      NM=llvm-nm \
 		      OBJCOPY=llvm-objcopy \
    		      OBJDUMP=llvm-objdump \
-                      CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
+                      CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log
 }
 
 function zupload()
 {
-rm -rf AnyKernel	
+rm -rf AnyKernel
 git clone --depth=1 https://github.com/AbzRaider/AnyKernel33 -b pissarro AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
 zip -r9 Test-OSS-KERNEL-PISSARRO-R.zip *
-curl --upload-file "Test-OSS-KERNEL-PISSARRO-R.zip" https://free.keep.sh
+curl --upload-file "Test-NoVA-Pissarro.zip" https://free.keep.sh
 }
 compile
 zupload
